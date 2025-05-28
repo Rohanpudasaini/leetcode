5c6cb921dfcd429062fa9affbe95dc82ee5f9ef6
Rohan <35792030+Rohanpudasaini@users.noreply.github.com>
2025-05-28 06:49:01 +0545

    Time: 12 ms (21.56%), Space: 19.2 MB (87.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-elementsingle-loop.py
@@ -0,0 +1,27 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # occurance = {}
+        # for num in nums:
+        #     if occurance.get(num):
+        #         occurance[num] += 1
+        #     else:
+        #         occurance[num] = 1
+        # max_occurance = 0
+        # max_value = 0
+        # for key, value in occurance.items():
+        #     if value > max_occurance:
+        #         max_occurance = value
+        #         max_value = key
+        # return max_value
+        occurance = {}
+        max_value = max_occurance = 0
+        for num in nums:
+            occurance[num] = 1 + occurance.get(num, 0)
+            if occurance[num] > max_occurance:
+                max_value = num
+                max_occurance = occurance[num]
+        return max_value
+
+            
+        
+        
