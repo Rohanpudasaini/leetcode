9a2fcff9c12c9f6a26f9b7bcb877bc35d948ad1e
Rohan <35792030+Rohanpudasaini@users.noreply.github.com>
2025-07-02 08:48:18 +0545

    Time: 0 ms (100%), Space: 17.7 MB (61.93%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,27 +1,29 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        visited = set()
-        final_order = []
+        row = len(matrix)
+        column = len(matrix[0])
+        result = []
+        top = 0
+        left =0
+        right = column-1
+        buttom = row-1
+        while top <= buttom and left <=right:
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            top += 1
+            for i in range(top, buttom+1):
+                result.append(matrix[i][right])
+            right -= 1
+            if top <= buttom:
+                for i in range(right, left-1, -1):
+                    result.append(matrix[buttom][i])
+                buttom -=1
 
-        def dfs(row, col, past_direction):
-            directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+            if left <=right:
+                for i in range(buttom, top-1, -1):
+                    result.append(matrix[i][left])
+                left +=1
 
-            # Update the list to the have the most recently travelled direction first 
-            directions.insert(0, directions.pop(directions.index(past_direction)))
 
-            for direction in directions:
-                n_row, n_col = row + direction[0], col + direction[1]
-
-                # Check if new coordinate is inbounds, and we have't visisited it already
-                if (n_row in range(len(matrix)) and
-                    n_col in range(len(matrix[0])) and
-                    (n_row, n_col) not in visited):
-                    visited.add((n_row, n_col))
-                    final_order.append(matrix[n_row][n_col])
-                    dfs(n_row, n_col, direction)
-
-        visited.add((0,0))
-        final_order.append(matrix[0][0])
-        dfs(0, 0, [0, 1])
-
-        return final_order
+        return result
+        
