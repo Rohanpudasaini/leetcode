442b8fea31812288912c6bda65c6117e25b5691c
Rohan <pudasainirohan0@gmail.com>
2026-01-14 12:54:52 +0545

    feat: factorial using recursion

--- recursion/print_something_n_times.py
+++ recursion/print_something_n_times.py
@@ -30,7 +30,17 @@ def sum_up_to_n(n: int, current: int = 0, sum: int = 0) -> int:
     return sum_up_to_n(n, current + 1, current + sum)
 
 
+# Factorial of N using recursion
+# Factorial of 3 = 3x2x1
+# Base case factorial of 0 is 1
+def factorial(n: int) -> int:
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
 print_n_times(5)
 print_to_n(5, 0)
 print_n_to_1(5)
 print(sum_up_to_n(4))
+print(factorial(10))
