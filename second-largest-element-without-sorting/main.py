f75f9087e7d30546359048c9e4c6e8f7664d74a5
Rohan <pudasainirohan0@gmail.com>
2026-01-28 13:22:04 +0545

    feat: second largest and smallest

--- second-largest-element-without-sorting/main.py
+++ second-largest-element-without-sorting/main.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 def second_largest(array: list[int]) -> int:
     largest = -2
     second_largest = -1
@@ -9,13 +12,13 @@ def second_largest(array: list[int]) -> int:
     return second_largest
 
 
-arry = [1, 2, 4, 7, 7, 5]
+arry = [1, 1, 2, 4, 7, 7, 5]
 
 print(second_largest(arry))
 
 
-def second_largest_without_duplicate(array: list[int]) -> int:
-    largest, second_largest = -2, -1
+def second_largest_without_duplicate(array: list[int]) -> Union[int, float]:
+    largest, second_largest = float("-inf"), float("-inf")
     for i in range(len(array)):
         if array[i] > largest:
             second_largest, largest = largest, array[i]
@@ -24,6 +27,24 @@ def second_largest_without_duplicate(array: list[int]) -> int:
     return second_largest
 
 
+def second_largest_and_smallest_without_duplicate(
+    array: list[int],
+) -> tuple[Union[int, float], Union[int, float]]:
+    largest, second_largest = float("-inf"), float("-inf")
+    smallest, second_smallest = float("inf"), float("inf")
+    for i in range(len(array)):
+        if array[i] > largest:
+            second_largest, largest = largest, array[i]
+        elif array[i] > second_largest and arry[i] != largest:
+            second_largest = array[i]
+        if array[i] < smallest:
+            second_smallest, smallest = smallest, array[i]
+        elif array[i] < second_smallest and array[i] != smallest:
+            second_smallest = array[i]
+    return second_largest, second_smallest
+
+
 # smallest, second_smallest = 10001, 10000
 
 print(second_largest_without_duplicate(arry))
+print(second_largest_and_smallest_without_duplicate(arry))
